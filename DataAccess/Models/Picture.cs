112c8ba001959e659b75133a6cabb7f107f91b40
sangviehuy <hvsang2101@gmail.com>
2018-08-27 22:10:13 +0700

    Initial project and create Register Account view.

---
+++ DataAccess/Models/Picture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Picture
+    {
+        public int PictureID { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public int AmountOfLove { get; set; }
+        public int Viewer { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string HashTag { get; set; }
+
+        public User Owner { get; set; }
+    }
+}
---
+++ Go4Life/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Go4Life.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Go4Life.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountRegisterViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
